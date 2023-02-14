352962795f83b73ce1796794c86c19f3ac80e8e7
James Goodhand <james.goodhand@hey.com>
2023-02-14 14:55:23 +0100

    Uncomment variables. (#12)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -24,8 +24,8 @@ module "playground" {
     source  = "jagoodhand/playground/snowflake"
     version = "0.0.6"
 
-    # data_retention_time = 0
-    # max_object_age_without_tag = 2
-    # tasks_enabled = true
-    # dry_run = true
+    data_retention_time = 0
+    max_object_age_without_tag = 2
+    tasks_enabled = true
+    dry_run = true
 }
